4ac673a83b95dab688333b484929dc961112ea64
Mariadel <mariadel52@correo.ugr.es>
2017-12-17 12:55:52 +0100

    Added n-body problem implementation

--- Planets.py
+++ Planets.py
@@ -1,12 +1,4 @@
-##############################################################################################
-#                                                                                            # 
-# Author: María del Mar Ruiz Martín                                                          # 
-#                                                                                            #                                   
-# Mecánica Celeste                                                                           #                                   
-#                                                                                            # 
-##############################################################################################
 from aux import *
-
 
 #Globar variables with constants information
 global planets
@@ -17,20 +9,157 @@ global Omega
 global omega
 global w
 global colors
+global mass
+global G_0
+global G
+global M
+global I
 
 planets = np.array(["Mercurio","Venus","Tierra", "Marte","Júpiter","Saturno","Urano","Neptuno"])
 a = np.array([0.387,0.723,1,1.524,5.203,9.546,19.2,30.09])
 epsilon = np.array([0.206,0.007,0.017,0.093,0.048,0.056,0.047,0.009])   
-period = np.array([87.97,224.7,365.26,686.98,4332.6,10759,30687,60784])
+period = np.array([87.97,224.7,365.26,686.98,4332.6,10759,30687,60194.84,])#60784])
+mass = np.array([3.301, 48.67, 59.72,6.417,18990,5685,868.2,1024])*10**23
 Omega = np.array([47.14,75.78,0,48.78,99.44,112.79,73.48,130.68])
 omega = np.array([75.9,130.15,101.22,334.22,12.72,91.09,169.06,43.83])
+M=1.9891*10**30   #Kg
+G_0=6.6738431*(10**-11)  #m3/kg/s**2
+G=G_0*(60*60*24)**2/(1.495978707*10**11)**3 #UA3/kg/día**2
+mu=M*G
 w = omega - Omega
 w = np.array([math.radians(i) for i in w])
+I = np.array([7, 3.59, 0, 1.85, 1.31, 2.5, 0.77, 1.78])
+I = np.array([math.radians(i) for i in I])
+Omega = np.array([math.radians(i) for i in Omega])
+omega = np.array([math.radians(i) for i in omega])
+
 colors = np.array(['rgb(100, 100, 100)','rgb(190, 100, 210)','rgb(0, 0, 250)','rgb(250, 0, 0)',
                     'rgb(77, 0, 77)','rgb(100, 30, 30)','rgb(260, 150, 0)','rgb(0, 150, 150)'])
 
+
 class Planet:
-    def __init__(self,name, a,epsilon, period,w, color):
+    def __init__(self,name, a,epsilon,Omega, w, i,mass, sun_mass, G, color):
+        self.name = name
+        self.a = a
+        self.epsilon = epsilon
+        self.mass = mass 
+        self.getMu(sun_mass, G)
+        self.calculatePeriod()
+        self.w = w
+        self.i = i
+        self.rot = np.array([[math.cos(self.w),-math.sin(self.w),0],[math.sin(self.w),math.cos(self.w),0],[0,0,1]])#El giro lo queremos hacer de \overline{\omega}
+        self.rot = np.dot(np.array([[math.cos(i)+math.cos(Omega)**2*(1-math.cos(i)), math.cos(Omega)*math.sin(Omega)*(1-math.cos(i)),-math.sin(Omega)*math.sin(i)],
+                    [math.cos(Omega)*math.sin(Omega)*(1-math.cos(i)),math.cos(i)+math.sin(Omega)**2*(1-math.cos(i)),-math.cos(Omega)*math.sin(i)],
+                    [math.sin(Omega)*math.sin(i), math.cos(Omega)*math.sin(i),math.cos(i)]]),self.rot)
+        self.getAngularMomentum_constant()
+
+        self.orbit = None
+        self.color = color
+
+    #Function to calculate period
+    def calculatePeriod(self):
+        self.period = self.a**1.5*2*math.pi/math.sqrt(self.mu)
+
+    #Function to get Eccentric Anomaly with Newton Raphson method
+    def getEccentricAnomaly(self,t):
+        return getEccentricAnomaly_Newton(t,self.period, self.epsilon)
+
+    #Function to get Eccentric Anomaly with Bessel's functions
+    def getEccentricAnomaly_Bessel(self,t):
+        return getEccentricAnomaly_Bessel(t,self.period,self.epsilon)
+
+    #Function to get the position of the given planet at time t
+    def getPosition(self,t): 
+        u = self.getEccentricAnomaly(t)
+        eps = self.epsilon
+        return np.dot(self.rot,self.a*np.array([math.cos(u)-eps,math.sqrt(1-eps**2)*math.sin(u),0]))
+    
+    #Function to get the distance of the given planet at time t
+    def getDistance(self,t):
+        return np.linalg.norm(self.getPosition(t))
+
+    #Auxiliar function to get mu given the planet index
+    def getMu(self, sun_mass, G):
+        self.mu = G*sun_mass**3 /(sun_mass + self.mass)**2
+
+    #Function to get the speed of a given planet at time t
+    def getSpeed(self,t):
+        u = self.getEccentricAnomaly(t)
+        eps = self.epsilon
+        return np.dot(self.rot,np.array([-math.sin(u),math.sqrt(1-eps**2)*math.cos(u),0])*math.sqrt(self.mu/self.a)/(1-eps*math.cos(u)))
+
+    #Function to get the speed modul of a given planet at time t
+    def getSpeedModul(self,t):
+        return np.linalg.norm(self.getSpeed(t))
+
+    #Function to get angular Momentum
+    def getAngularMomentum(self,t):
+        return np.cross(self.getPosition(t),self.getSpeed(t))
+        #return np.dot(self.rot,np.cross(self.getPosition(t),self.getSpeed(t)))
+    
+    #Function to get angular Momentum
+    def getAngularMomentum_constant(self):
+        #self.c = np.dot(self.rot,np.array([0,0,math.sqrt(self.mu*self.a*(1-self.epsilon**2))]))
+        self.c = np.dot(self.rot,np.array([0,0,math.sqrt(self.mu*self.a*(1-self.epsilon**2))]))
+
+    #Funtion to get theta derivative given theta
+    def theta_deriv(self,t,theta):
+        return np.linalg.norm(self.c)/(self.a**2*(1-self.epsilon**2)**2)*(1+self.epsilon*math.cos(theta))**2
+
+    #Function to get Real Anomaly with Runge Kutta method
+    def getRealAnomaly(self,t):
+        return runge_Kutta(t%self.period,self.theta_deriv)
+
+    #Function to get position using firts equation sistem
+    def get_position_S1(self,t):
+        theta = self.getRealAnomaly(t)
+        return np.array([math.cos(theta+self.w),math.sin(theta+self.w)])*self.a*(1-self.epsilon**2)/(1+self.epsilon*math.cos(theta))
+
+    #Function to get Energy of a planet
+    def getEnergy(self,t):
+        kineticEnergy = 0.5*self.getSpeedModul(t)**2
+        potentialEnergy = -self.mu/self.getDistance(t)
+        return kineticEnergy + potentialEnergy
+    
+    #Function to get Energy of a planet with the constant expression
+    def getEnergy_constant(self):
+        return -np.linalg.norm(self.c)**2/(2*self.a**2*(1-self.epsilon**2))
+
+    #Function to get the Real anomaly of a given planet given eccentric anomaly
+    def getRealAnomaly_2(self,u):
+        n = u//(math.pi*2)
+        eps = self.epsilon
+        theta = math.acos((math.cos(u)-eps)/(1-eps*math.cos(u)))
+
+        if u%(2*math.pi) > math.pi:
+            theta =  2*math.pi-theta
+        return theta +  2*n*math.pi
+
+    #Function to calculate the orbit of the planet
+    def getOrbit(self):
+        if self.orbit is None:
+            N = 500
+            self.orbit = np.asarray([self.getPosition(self.period*i/N) for i in range(N)]) 
+        return self.orbit
+
+    #Function to calculate N points of the planet's orbit
+    def getPoints(self,N):
+        return  np.asarray([self.getPosition(self.period*i/N) for i in range(N)]) 
+
+    def __str__(self):
+        return self.name
+
+
+class SolarSistem:
+    def __init__(self):
+        self.planets = []
+        self.mass = M
+        for i in range(len(planets)):
+            self.planets.append(Planet(planets[i],a[i],epsilon[i],Omega[i],omega[i],I[i],mass[i],self.mass,G,colors[i]))  
+
+
+class Planet_2:
+    def __init__(self,name, a,epsilon, period,Omega, w,i, color):
         self.name = name
         self.a = a
         self.epsilon = epsilon
@@ -38,7 +167,11 @@ class Planet:
         self.getMu()
         self.getAngularMomentum_constant()
         self.w = w
-        self.rot = np.array([[math.cos(self.w),-math.sin(self.w)],[math.sin(self.w),math.cos(self.w)]])
+        self.i = i
+        self.rot = np.array([[math.cos(self.w),-math.sin(self.w),0],[math.sin(self.w),math.cos(self.w),0],[0,0,1]])
+        self.rot = np.dot(np.array([[math.cos(i)+math.cos(Omega)**2*(1-math.cos(i)), math.cos(Omega)*math.sin(Omega)*(1-math.cos(i)),-math.sin(Omega)*math.sin(i)],
+                    [math.cos(Omega)*math.sin(Omega)*(1-math.cos(i)),math.cos(i)+math.sin(Omega)**2*(1-math.cos(i)),-math.cos(Omega)*math.sin(i)],
+                    [math.sin(Omega)*math.sin(i), math.cos(Omega)*math.sin(i),math.cos(i)]]),self.rot)
         self.orbit = None
         self.color = color
 
@@ -54,7 +187,7 @@ class Planet:
     def getPosition(self,t): 
         u = self.getEccentricAnomaly(t)
         eps = self.epsilon
-        return np.dot(self.rot,self.a*np.array([math.cos(u)-eps,math.sqrt(1-eps**2)*math.sin(u)]))
+        return np.dot(self.rot,self.a*np.array([math.cos(u)-eps,math.sqrt(1-eps**2)*math.sin(u),0]))
     
     #Function to get the distance of the given planet at time t
     def getDistance(self,t):
@@ -68,7 +201,7 @@ class Planet:
     def getSpeed(self,t):
         u = self.getEccentricAnomaly(t)
         eps = self.epsilon
-        return np.dot(self.rot,np.array([-math.sin(u),math.sqrt(1-eps**2)*math.cos(u)])*math.sqrt(self.mu/self.a)/(1-eps*math.cos(u)))
+        return np.dot(self.rot,np.array([-math.sin(u),math.sqrt(1-eps**2)*math.cos(u),0])*math.sqrt(self.mu/self.a)/(1-eps*math.cos(u)))
 
     #Function to get the speed modul of a given planet at time t
     def getSpeedModul(self,t):
@@ -76,7 +209,9 @@ class Planet:
 
     #Function to get angular Momentum
     def getAngularMomentum(self,t):
-        return np.array([0,0,np.cross(self.getPosition(t),self.getSpeed(t))])
+        return np.cross(self.getPosition(t),self.getSpeed(t))
+
+        #return np.array([0,0,np.cross(self.getPosition(t),self.getSpeed(t))])
     
     #Function to get angular Momentum
     def getAngularMomentum_constant(self):
@@ -129,8 +264,8 @@ class Planet:
         return self.name
 
 
-class SolarSistem:
+class SolarSistem_2:
     def __init__(self):
         self.planets = []
         for i in range(len(planets)):
-            self.planets.append(Planet(planets[i],a[i],epsilon[i],period[i],w[i],colors[i]))  
+            self.planets.append(Planet_2(planets[i],a[i],epsilon[i],period[i],Omega[i],omega[i],I[i], colors[i]))  
--- aux.py
+++ aux.py
@@ -4,6 +4,13 @@ import random
 import scipy.special as ss
 import plotly.offline as py
 import plotly.graph_objs as go
+from datetime import datetime as dt
+
+
+global M
+M=1.9891*10**30   #Kg
+
+
 
 #Auxiliar function to get u_{n+1}
 def phi(u_ant,gi,eps):
@@ -30,13 +37,28 @@ def getEccentricAnomaly_Newton(t, period, eps):
     u_0 = math.pi
     tol = 0.00001
 
-    gi = 2*math.pi*t/period
-    u_ant, u_n = getInitialPoint(u_0,t,period,gi,eps)
+    ji = 2*math.pi*t/period
+    nsv = int(ji/math.pi)  
+
+    if nsv % 2 == 0:
+        ji -= math.pi*nsv 
+        ind = 1
+        a = 0
+    else:
+        ji -= math.pi*nsv + math.pi
+        ji = -ji
+        ind = -1
+        a = 1
+        
+    #u_ant, u_n = getInitialPoint(u_0,t,period,gi,eps)
+    u_ant = 0
+    u_n = math.pi
 
     while(abs(u_n-u_ant) > tol):
         u_ant = u_n
-        u_n = phi(u_ant,gi,eps)
+        u_n = phi(u_ant,ji,eps)
 
+    u_n  = u_n * ind + (nsv+a)*math.pi
     return u_n 
 
 
@@ -74,33 +96,197 @@ def runge_Kutta(t,f):
         t_i += h  
     return theta_n        
 
+
+def getSunPosition(planet_pos, planet_mass, sun_mass):
+    return -planet_mass/sun_mass*planet_pos
+
+
+def translate(v,a,b,t):
+    return v + a + b*t
+
+
+
 #Function to plot the orbit of the planet
-def display_orbit(planet, t, opt, N):
-    position = planet.getPosition(t)   
-    planet_pos = go.Scattergl(x=[position[0]], y=[position[1]], name=planet.name, mode='markers', marker=dict(size=25,color=planet.color,line=dict(width=1, color=planet.color)))  
+def display_orbit(planet, t, opt, N, a,b):
+    position = translate(planet.getPosition(t),a,b,t)
+    planet_pos = go.Scatter3d(x=[position[0]], y=[position[1]], z = [position[2]], name=planet.name, mode='markers', marker=dict(size=5,color=planet.color,line=dict(width=1, color=planet.color)))  
 
     if opt:
         coordinates = planet.getOrbit()
-        orbit = go.Scattergl(x=coordinates[:, 0], y=coordinates[:, 1], name="órbita de "+ planet.name,mode='lines', marker = dict(color = 'rgb(0,0,0)',size=3,line = dict(width = 0)))
+        coordinates = np.array([translate(i,a,b,t) for i in coordinates])
+        orbit = go.Scatter3d(x=coordinates[:, 0], y=coordinates[:, 1],z = coordinates[:,2], name="órbita de "+ planet.name,mode='lines', marker = dict(color = 'rgb(0,0,0)',size=3,line = dict(width = 0)))
+    else:
+        coordinates = planet.getPoints(N)
+        coordinates = np.array([translate(i,a,b,t) for i in coordinates])
+        
+        orbit = go.Scatter3d(x=coordinates[:, 0], y=coordinates[:, 1], z = coordinates[:,2],name="órbita de "+ planet.name,mode='markers', marker = dict(color = 'rgb(0,0,0)',size=1,line = dict(width = 0)))
+
+    sun_poss = translate(getSunPosition(position, planet.mass, M),a,b,t)
+    print("Posición del Sol: ("+str(sun_poss[0])+", "+str(sun_poss[1])+", "+str(sun_poss[2])+") ")
+    sun_speed = -planet.getSpeed(t)*planet.mass/M
+    print("Velocidad del Sol: ("+str(sun_speed[0])+", "+str(sun_speed[1])+", "+str(sun_speed[2])+") ")
+
+    sun = go.Scatter3d(x = [sun_poss[0]], y = [sun_poss[1]], z = [sun_poss[2]],name='Sol',mode='markers',marker=dict(size=15,color='rgb(250, 250, 0)',line=dict(width=1, color='rgb(100,100,0)')))
+
+    #r = int(planet.a)*2
+    r = np.max(coordinates,axis = 0)
+    r = np.max(r)
+    r_2 = np.min(coordinates, axis = 0)
+    r_2 = np.min(r_2)
+    layout = go.Layout(
+                    scene = dict(
+                    xaxis = dict(
+                        nticks=4, range = [r_2,r],),
+                    yaxis = dict(
+                        nticks=4, range = [r_2,r],),
+                    zaxis = dict(
+                        nticks=4, range = [r_2,r],),),
+                    width=700,
+                    margin=dict(
+                    r=20, l=10,
+                    b=10, t=10)
+                  )
+
+
+    r_3 = np.max(coordinates, axis = 0)
+    r_4 = np.min(coordinates, axis = 0)
+    r_5 = np.max(r_3 - r_4)
+    dif = np.array([r_5 for i in range(3)]) - (r_3 - r_4)
+
+    
+    #layout = go.Layout(
+    #                scene = dict(
+    #                xaxis = dict(
+    #                    nticks=4, range = [r_4[0] - dif[0]/2,r_4[0] + dif[0]/2],),
+    #                yaxis = dict(
+    #                    nticks=4, range = [r_4[1] - dif[1]/2,r_4[1] + dif[1]/2],),
+    #                zaxis = dict(
+    #                    nticks=4, range = [r_4[2] - dif[2]/2,r_4[2] + dif[2]/2],),),
+    #                width=700,
+    #                margin=dict(
+    #                r=20, l=10,
+    #                b=10, t=10)
+    #              )
+    #layout = go.Layout(width=1000, height=700,xaxis=dict(anchor='y',range=[-r, r]), yaxis=dict(anchor='x',autorange=True,range=[-r, r],))
+
+    data = [sun, orbit,planet_pos]
+    fig = go.Figure(data=data, layout=layout)
+    py.iplot(fig) 
+
+def getC_m(m,M,x_0,x_1,y_1):
+    alpha = m/(m+M)*x_0
+    beta = m/(m+M)*x_1 + m/(m+M)*y_1 - alpha
+    return alpha, beta
+
+def getC_m_2(m,M,x_0,speed_0):
+    alpha = m/(m+M)*x_0
+    beta = m/(m+M)*speed_0
+    return alpha, beta
+
+
+#Function to display Info
+def showInfo(planet,t,u,opt,N):
+    sol_1 = np.array([0.001, 0.001, 0.001])
+    alpha , beta = getC_m_2(planet.mass, M, planet.getPosition(0), planet.getPosition(1))
+    print("Información para ", planet.name," en el día ", t)    
+    #t = t%planet.period
+    print("Posición: ", translate(planet.getPosition(t),alpha,beta,t))
+    print("Distancia al Sol: ", planet.getDistance(t))
+    print("Vector velocidad: ", planet.getSpeed(t))
+    print("Módulo del vector velocidad: ", planet.getSpeedModul(t))
+    print("Anomalía real: ", planet.getRealAnomaly(t))
+    print("Energía total (expresión en función del tiempo): ", planet.getEnergy(t))
+    print("Energía total (expresión constante): ", planet.getEnergy_constant())
+    print("Momento angular (expresión en función del tiempo): ", planet.getAngularMomentum(t))
+    print("Momento angular (expresión constante): ", planet.c)
+
+    print("Para una anomalía excéntrica de ", u, "la anomalía real es: ",planet.getRealAnomaly_2(u))
+
+    print("Anomalía excéntrica (mediante el método de Newton-Raphson): ",planet.getEccentricAnomaly(t))
+    print("Anomalía excéntrica (mediante funciones de Bessel): ",planet.getEccentricAnomaly_Bessel(t))
+    print("La diferencia obtenida entre ambos métodos es: ", abs(planet.getEccentricAnomaly(t) - planet.getEccentricAnomaly_Bessel(t)))
+    print("El centro de masas es: ", alpha + beta*t)
+
+    display_orbit(planet,t,opt,N,alpha,beta)
+
+
+
+
+
+
+
+
+#Function to plot the orbit of the planet
+def display_orbit_2(planet, t, opt, N):
+    position = planet.getPosition(t)
+    planet_pos = go.Scatter3d(x=[position[0]], y=[position[1]], z = [position[2]], name=planet.name, mode='markers', marker=dict(size=5,color=planet.color,line=dict(width=1, color=planet.color)))  
+
+    if opt:
+        coordinates = planet.getOrbit()
+        orbit = go.Scatter3d(x=coordinates[:, 0], y=coordinates[:, 1],z = coordinates[:,2], name="órbita de "+ planet.name,mode='lines', marker = dict(color = 'rgb(0,0,0)',size=3,line = dict(width = 0)))
     else:
         coordinates = planet.getPoints(N)
         
-        orbit = go.Scattergl(x=coordinates[:, 0], y=coordinates[:, 1], name="órbita de "+ planet.name,mode='markers', marker = dict(color = 'rgb(0,0,0)',size=6,line = dict(width = 0)))
+        orbit = go.Scatter3d(x=coordinates[:, 0], y=coordinates[:, 1], z = coordinates[:,2],name="órbita de "+ planet.name,mode='markers', marker = dict(color = 'rgb(0,0,0)',size=1,line = dict(width = 0)))
 
-    sun = go.Scattergl(x=[0], y=[0],name='Sol',mode='markers',marker=dict(size=35,color='rgb(250, 250, 0)',line=dict(width=1, color='rgb(100,100,0)')))
+    sun_poss = np.array([0,0,0])
 
-    r = int(planet.a)*2
-    layout = go.Layout(width=1000, height=700,xaxis=dict(anchor='y',range=[-r, r]), yaxis=dict(anchor='x',autorange=False,range=[-r, r],))
+    sun = go.Scatter3d(x = [sun_poss[0]], y = [sun_poss[1]], z = [sun_poss[2]],name='Sol',mode='markers',marker=dict(size=15,color='rgb(250, 250, 0)',line=dict(width=1, color='rgb(100,100,0)')))
+
+    #r = int(planet.a)*2
+    r = np.max(coordinates,axis = 0)
+    r = np.max(r)
+    r_2 = np.min(coordinates, axis = 0)
+    r_2 = np.min(r_2)
+    layout = go.Layout(
+                    scene = dict(
+                    xaxis = dict(
+                        nticks=4, range = [r_2,r],),
+                    yaxis = dict(
+                        nticks=4, range = [r_2,r],),
+                    zaxis = dict(
+                        nticks=4, range = [r_2,r],),),
+                    width=700,
+                    margin=dict(
+                    r=20, l=10,
+                    b=10, t=10)
+                  )
+
+
+    r_3 = np.max(coordinates, axis = 0)
+    r_4 = np.min(coordinates, axis = 0)
+    r_5 = np.max(r_3 - r_4)
+    dif = np.array([r_5 for i in range(3)]) - (r_3 - r_4)
+
+    
+    #layout = go.Layout(
+    #                scene = dict(
+    #                xaxis = dict(
+    #                    nticks=4, range = [r_4[0] - dif[0]/2,r_4[0] + dif[0]/2],),
+    #                yaxis = dict(
+    #                    nticks=4, range = [r_4[1] - dif[1]/2,r_4[1] + dif[1]/2],),
+    #                zaxis = dict(
+    #                    nticks=4, range = [r_4[2] - dif[2]/2,r_4[2] + dif[2]/2],),),
+    #                width=700,
+    #                margin=dict(
+    #                r=20, l=10,
+    #                b=10, t=10)
+    #              )
+    #layout = go.Layout(width=1000, height=700,xaxis=dict(anchor='y',range=[-r, r]), yaxis=dict(anchor='x',autorange=True,range=[-r, r],))
 
     data = [sun, orbit,planet_pos]
     fig = go.Figure(data=data, layout=layout)
     py.iplot(fig) 
 
 
+
+
 #Function to display Info
-def showInfo(planet,t,u,opt,N):
+def showInfo_2(planet,t,u,opt,N):
+    alpha = np.array([0,0,0.1])
+    beta = np.array([0,0.2,0.5])
     print("Información para ", planet.name," en el día ", t)    
-    t = t%planet.period
+    #t = t%planet.period
     print("Posición: ", planet.getPosition(t))
     print("Distancia al Sol: ", planet.getDistance(t))
     print("Vector velocidad: ", planet.getSpeed(t))
@@ -115,6 +301,18 @@ def showInfo(planet,t,u,opt,N):
 
     print("Anomalía excéntrica (mediante el método de Newton-Raphson): ",planet.getEccentricAnomaly(t))
     print("Anomalía excéntrica (mediante funciones de Bessel): ",planet.getEccentricAnomaly_Bessel(t))
-    print("La diferencia obtenida entre ambos métodos es: ",abs(planet.getEccentricAnomaly(t) - planet.getEccentricAnomaly_Bessel(t)))
+    print("La diferencia obtenida entre ambos métodos es: ", abs(planet.getEccentricAnomaly(t) - planet.getEccentricAnomaly_Bessel(t)))
 
-    display_orbit(planet,t,opt,N)
+    display_orbit_2(planet,t,opt,N)
+
+
+
+t_ini = dt(1900,1,1)
+
+
+#Function to change dato to time
+def date2Time(date):
+    formato = "%d/%m/%Y"
+    return (dt.strptime(t,formato)-t_ini).days
+
+
